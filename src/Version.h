c668937f7c7782e15c1c1376d5534f2937e32b9c
philres <philipp.rescheneder@univie.ac.at>
2015-11-24 20:44:30 +0100

    Version

--- src/Version.h
+++ src/Version.h
@@ -1,8 +1,9 @@
 #ifndef VERSION_H
 #define VERSION_H
-
+ 
 #define VERSION_MAJOR "0"
 #define VERSION_MINOR "4"
-#define VERSION_BUILD "13-slambr"
+#define VERSION_BUILD "13"
 
 #endif // VERSION_H
+
